ff85886b5ba7088ab5e04a03847aeee9521b909a
dianlujitao <dianlujitao@lineageos.org>
2018-10-21 15:37:35 +0800

    libdisplayconfig: Depend on vendor.display.config@1.0_vendor
    Change-Id: I65549e8d3a41cb85668eeb5302c8f79082a6838c

--- libdisplayconfig/Android.mk
+++ libdisplayconfig/Android.mk
@@ -7,7 +7,7 @@ LOCAL_C_INCLUDES              := $(common_includes)
 LOCAL_HEADER_LIBRARIES        := display_headers
 LOCAL_SRC_FILES               := DisplayConfig.cpp
 LOCAL_SHARED_LIBRARIES        := libhidlbase libhidltransport libutils \
-                                 vendor.display.config@1.0
+                                 vendor.display.config@1.0_vendor
 LOCAL_EXPORT_C_INCLUDE_DIRS   := $(LOCAL_PATH)
 
 include $(BUILD_SHARED_LIBRARY)
